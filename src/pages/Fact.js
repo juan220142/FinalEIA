4aba059df2c4efb0c64978222dc9bddcd4244446
juanf <juan220142@gmail.com>
2021-11-12 14:46:03 -0500

    hola

--- src/pages/Fact.js
+++ src/pages/Fact.js
@@ -1,8 +1,36 @@
 import React,{Component} from "react";
 import "../assets/css/base.css"
 import {Link} from "react-router-dom";
-
+import {Axios} from "axios"
 class Facts extends Component{
+    constructor() {
+        super();
+        this.state ={
+            total:0
+        }
+        this.onFinish=this.onFinish.bind(this)
+    }
+
+    async onFinish(values) {
+
+        console.log(values)
+
+        const url = 'localhost:5000/api/fact'
+
+        const set = {
+            method: 'get',
+            url: url,
+            headers:{
+                'dato':values.dato
+            }
+        };
+
+        const response = await Axios(set)
+
+        const data = response.data.data
+
+        this.setState({total:data})
+    }
     render(){
         return(
             <div className="base">
@@ -17,6 +45,15 @@ class Facts extends Component{
                         <Link to="/"><button>Volver</button></Link>
                     </form>
                 </div>
+                {this.state.total > 0
+                &&(
+                    <div>
+                        <div className="titleHolder">
+                            <h2>Resultado{this.state.total}</h2>
+                        </div>
+                    </div>
+                )
+                }
             </div>
 
         );
