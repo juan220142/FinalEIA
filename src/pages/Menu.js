cb9452d3a7134d76e0f8a131f9937dc2f510ebee
juanf <juan220142@gmail.com>
2021-11-13 18:22:23 -0500

    fixe space on the base of the css to won't have out words

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 class Menu extends Component{
     render(){
         return(
+
                 <div className="fondo">
                 <div className="base">
                     <div className="div1">
